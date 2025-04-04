9b6c5d9b0de8ee9c6606539edf79b4f0b74701aa
Marvy247 <marvellousdvd@gmail.com>
2025-04-04 14:46:07 +0100

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],

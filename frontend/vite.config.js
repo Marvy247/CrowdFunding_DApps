73a6bfd1f8c6d8cba49f271e61cb240701026373
Marvy247 <marvellousdvd@gmail.com>
2025-04-04 13:03:07 +0100

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       "aos/dist/aos.css": path.resolve(
         __dirname,
         "node_modules/aos/dist/aos.css"
       ),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "${path.resolve(
-          __dirname,
-          "node_modules/aos/dist/aos.css"
-        )}";`,
-      },
     },
   },
 });
